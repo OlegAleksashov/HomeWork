323d415835a83e6a020d215b5c0a81c76d94795b
Oleg Aleksashov <Fairp87@gmail.com>
2022-03-22 22:09:42 +0300

    first commit

---
+++ src/home_work_1/task_1_2.java
@@ -0,0 +1,26 @@
+package home_work_1;
+
+public class task_1_2 {
+    public static void main(String[] args) {
+        int a = 42; //
+        int b = 15;
+        int c = a & b;
+        int d = a | b;
+        int s = ~ a;
+        int g = a ^ b;
+        int f = a &= b;
+        int r = a |= b;
+        int p = a ^ b;
+        int i = a ^= b;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(g);
+        System.out.println(f);
+        System.out.println(r);
+        System.out.println(p);
+        System.out.println(i);
+
+    }
+}
